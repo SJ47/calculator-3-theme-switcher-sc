9021a990298866835facda8bd84e1f482799f4fa
Scott Johnston <scott@umachan.co.uk>
2021-10-17 17:21:33 +0100

    Moved themes to own file

--- src/containers/CalculatorContainer.jsx
+++ src/containers/CalculatorContainer.jsx
@@ -2,61 +2,7 @@ import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import Header from "../components/header/Header";
 import { StyledContainer } from "./CalculatorContainer.styled";
-
-const darkTheme = {
-    colors: {
-        bgMain: "var(--color-dt-main-bg)",
-        bgToggle: "var(--color-dt-toggle-bg)",
-        bgScreen: "var(--color-dt-screen-bg)",
-        keyBg1: "var(--color-dt-key-bg-1)",
-        keyShadow1: "var(--color-dt-shadow-bg-1)",
-        keyBg2: "var(--color-dt-key-bg-2)",
-        keyShadow2: "var(--color-dt-shadow-bg-2)",
-        keyBg3: "var(--color-dt-key-bg-3)",
-        keyShadow3: "var(--color-dt-shadow-bg-3)",
-        text1: "var(--color-dt-text-1)",
-        text2: "var(--color-dt-text-2)",
-    },
-    mobile: "768px",
-    translateX: "2px",
-};
-
-const lightTheme = {
-    colors: {
-        bgMain: "var(--color-lt-main-bg)",
-        bgToggle: "var(--color-lt-toggle-bg)",
-        bgScreen: "var(--color-lt-screen-bg)",
-        keyBg1: "var(--color-lt-key-bg-1)",
-        keyShadow1: "var(--color-lt-shadow-bg-1)",
-        keyBg2: "var(--color-lt-key-bg-2)",
-        keyShadow2: "var(--color-lt-shadow-bg-2)",
-        keyBg3: "var(--color-lt-key-bg-3)",
-        keyShadow3: "var(--color-lt-shadow-bg-3)",
-        text1: "var(--color-lt-text-1)",
-        text2: "var(--color-lt-text-2)",
-    },
-    mobile: "768px",
-    translateX: "20px",
-};
-
-const purpleTheme = {
-    colors: {
-        bgMain: "var(--color-purple-main-bg)",
-        bgToggle: "var(--color-purple-toggle-bg)",
-        bgScreen: "var(--color-purple-screen-bg)",
-        keyBg1: "var(--color-purple-key-bg-1)",
-        keyShadow1: "var(--color-purple-shadow-bg-1)",
-        keyBg2: "var(--color-purple-key-bg-2)",
-        keyShadow2: "var(--color-purple-shadow-bg-2)",
-        keyBg3: "var(--color-purple-key-bg-3)",
-        keyShadow3: "var(--color-lt-shadow-bg-3)",
-        text1: "var(--color-purple-text-1)",
-        text2: "var(--color-purple-text-2)",
-        text3: "var(--color-purple-text-3)",
-    },
-    mobile: "768px",
-    translateX: "42px",
-};
+import { darkTheme, lightTheme, purpleTheme } from "./MyThemes";
 
 const CalculatorContainer = () => {
     const [theme, setTheme] = useState(darkTheme);
